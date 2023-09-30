5471368b701bf4d74bf71015b460c33c26c2e50e
Jacob Lang <jacoblang72@comcast.net>
2023-09-30 15:48:38 -0400

    finished get all for project

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,24 @@
 // Write your "projects" router here!
+const express = require("express");
+const ProjectData = require("./projects-model");
+const {} = require("./projects-middleware");
+
+const router = express.Router();
+
+//get
+router.get("/",async(req,res,next)=>{
+    try {
+        const project = await ProjectData.get();
+        res.status(200).json(project)
+    } catch (err) {next(err)}
+})
+//get
+
+
+router.use((error,req,res,next)=> {
+    res.status(error.status || 500).json({
+        message : error.message,
+        customMessage  : "error in projects route"
+    })
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 //pull in routes
-
+const ProjectRoute = require("./projects/projects-router");
 //pull in routes
 
 
@@ -19,7 +19,7 @@ server.use(express.json());
 
 
 //server use routes
-
+server.use("/api/projects", ProjectRoute)
 //server use routes
 
 module.exports = server;
